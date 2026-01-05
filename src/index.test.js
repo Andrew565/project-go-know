ff20e441670e46cdcd972920cf00459545962dd7
Andrew Steele <andrew565@gmail.com>
2026-01-05 07:04:58 -0500

    Fixing test typing issues

--- src/index.test.js
+++ src/index.test.js
@@ -74,6 +74,21 @@ jest.mock('@andrewscripts/deck-of-cards.js', () => {
     };
 });
 
+/**
+ * @param {{numberRank: number, nameRank: string, suit: string}} card
+ */
+const makeACard = ({numberRank, nameRank, suit}) => {
+    return {
+        numberRank,
+        nameRank,
+        suit,
+        name: `${nameRank} of ${suit}`,
+        value: numberRank,
+        initial: nameRank[0],
+        facingDown: false
+    };
+};
+
 const {
   NewGame,
   Piles,
@@ -114,11 +129,11 @@ describe('Game Logic', () => {
 
   test('updateCounters should calculate score correctly', () => {
     // Manually set state to unpredictable but known values
-    Piles.thresholdActive = { value: 10, suit: 'Hearts', nameRank: '10' };
-    Piles.obstaclesActive = [{ value: 5, suit: 'Spades' }];
+    Piles.thresholdActive = makeACard({ numberRank: 10, suit: 'Hearts', nameRank: '10' });
+    Piles.obstaclesActive = [makeACard({ numberRank: 5, suit: 'Spades', nameRank: '5' })];
 
     // Select some cards
-    Piles.inventory['i1'].card = { value: 5, suit: 'Hearts' };
+    Piles.inventory['i1'].card = makeACard({ numberRank: 5, suit: 'Hearts', nameRank: '5' });
     Piles.inventory['i1'].selected = true;
 
     updateCounters();
@@ -134,11 +149,11 @@ describe('Game Logic', () => {
   test('playCards should move cards to hold if score is sufficient', () => {
      // Setup a winning scenario
      // Knowble: Value 2
-     Piles.thresholdActive = { value: 2, suit: 'Clubs', nameRank: '2' };
-     Piles.obstaclesActive = [{ value: 1, suit: 'Spades', nameRank: 'Ace' }]; // Must have obstacle
+     Piles.thresholdActive = makeACard({ numberRank: 2, suit: 'Clubs', nameRank: '2' });
+     Piles.obstaclesActive = [makeACard({ numberRank: 1, suit: 'Spades', nameRank: 'Ace' })]; // Must have obstacle
 
      // Inventory: Select a card worth 5 (Clubs)
-     Piles.inventory['i1'].card = { value: 5, suit: 'Clubs' };
+     Piles.inventory['i1'].card = makeACard({ numberRank: 5, suit: 'Clubs', nameRank: '5' });
      Piles.inventory['i1'].selected = true;
 
      // Mock updateCounters to ensure button enabling logic (though playCards calls logic directly)
@@ -149,34 +164,34 @@ describe('Game Logic', () => {
 
      // Verify:
      // Knowble should be in hold
-     expect(Piles.hold).toContainEqual({ value: 2, suit: 'Clubs', nameRank: '2' });
+     expect(Piles.hold).toContainEqual(makeACard({ numberRank: 2, suit: 'Clubs', nameRank: '2' }));
      // Inventory card should be discarded (moved to inventoryDiscard or handled)
      // Actually playCards pushes to inventoryDiscard and draws new card
-     expect(Piles.inventoryDiscard).toContainEqual({ value: 5, suit: 'Clubs' });
+     expect(Piles.inventoryDiscard).toContainEqual(makeACard({ numberRank: 5, suit: 'Clubs', nameRank: '5' }));
      expect(Piles.inventory['i1'].selected).toBe(false);
-     expect(Piles.inventory['i1'].card).not.toEqual({ value: 5, suit: 'Clubs' }); // Should be new card
+     expect(Piles.inventory['i1'].card).not.toEqual(makeACard({ numberRank: 5, suit: 'Clubs', nameRank: '5' })); // Should be new card
   });
 
   test('playCards should discard knowble if score is insufficient', () => {
       // Setup a losing scenario
-      Piles.thresholdActive = { value: 10, suit: 'Hearts', nameRank: '10' };
-      Piles.obstaclesActive = [{ value: 1, suit: 'Spades', nameRank: 'Ace' }];
+      Piles.thresholdActive = makeACard({ numberRank: 10, suit: 'Hearts', nameRank: '10' });
+      Piles.obstaclesActive = [makeACard({ numberRank: 1, suit: 'Spades', nameRank: 'Ace' })];
 
       // Select card worth 2 (Hearts)
-      Piles.inventory['i1'].card = { value: 2, suit: 'Hearts' };
+      Piles.inventory['i1'].card = makeACard({ numberRank: 2, suit: 'Hearts', nameRank: '2' });
       Piles.inventory['i1'].selected = true;
 
       playCards();
 
       // Knowble should be in thresholdDiscard
-      expect(Piles.thresholdDiscard).toContainEqual({ value: 10, suit: 'Hearts', nameRank: '10' });
+      expect(Piles.thresholdDiscard).toContainEqual(makeACard({ numberRank: 10, suit: 'Hearts', nameRank: '10' }));
       // Inventory card discarded
-      expect(Piles.inventoryDiscard).toContainEqual({ value: 2, suit: 'Hearts' });
+      expect(Piles.inventoryDiscard).toContainEqual(makeACard({ numberRank: 2, suit: 'Hearts', nameRank: '2' }));
   });
 
   test('drawObstaclesFor should draw 2 obstacles for King of Clubs', () => {
       // Create a King of Clubs
-      const kingClubs = { nameRank: 'King', suit: 'Clubs', value: 13 };
+      const kingClubs = makeACard({ numberRank: 13, suit: 'Clubs', nameRank: 'King' });
 
       // Ensure veil has enough cards
       // Piles.veil.drawPile is populated by NewGame/InitPiles
@@ -187,7 +202,7 @@ describe('Game Logic', () => {
   });
 
   test('drawObstaclesFor should draw 1 obstacle for non-King of Clubs', () => {
-      const queenHearts = { nameRank: 'Queen', suit: 'Hearts', value: 12 };
+      const queenHearts = makeACard({ numberRank: 12, suit: 'Hearts', nameRank: 'Queen' });
 
       drawObstaclesFor(queenHearts);
 
